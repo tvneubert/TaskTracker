d6f90b749bb7a8ed1e3acdb9a74175be1479e319
lilieemi <lilieemi@students.zhaw.ch>
2023-04-13 21:45:43 +0200

    Initial project class draft

---
+++ app/src/main/java/ch/zhaw/prog2/tasktracker/Project.java
@@ -0,0 +1,56 @@
+package ch.zhaw.prog2.tasktracker;
+
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+
+public class Project {
+    //TODO Replace Object with the proper Task class
+    private ArrayList<Object> tasks = new ArrayList<>();
+    //TODO Replace float with the proper Time
+    private float time;
+    boolean complete = false;
+
+    public boolean isCompleted(){return complete;}
+    private void calculateTime(){
+        for(Object task : tasks){
+            //Placeholder TODO replace with proper method
+            time += task.time();
+        }
+    }
+    //TODO Replace return value with proper type
+    public float getTime(){
+        calculateTime();
+        return time;
+    }
+    public ArrayList<Object> getOpenTasks(){
+        ArrayList<Object> openTasks = new ArrayList<>();
+        for(Object task : tasks){
+            if(task.isOpen){
+                openTasks.add(task);
+            }
+        }
+        return openTasks;
+    }
+    public void addTask(Object task){
+        if(complete){
+            complete = !complete;
+
+        }
+        tasks.add(task);
+    }
+    public ArrayList<Object> getTasks(){return tasks;}
+    public ArrayList<Object> getClosedTasks(){
+        ArrayList<Object> closedTasks = new ArrayList<>();
+        for(Object task : tasks){
+            if(!task.isOpen){
+                closedTasks.add(task);
+            }
+        }
+        return closedTasks;
+    }
+    public void removeTask (Object task){
+        tasks.remove(task);
+    }
+
+}
