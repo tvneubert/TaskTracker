0ea1eb0554ef06b5086341694a582619d4a6a575
lilieemi <lilieemi@students.zhaw.ch>
2023-05-07 16:46:10 +0200

    Removed Mock objects to allow serialization

--- app/src/test/java/ch/zhaw/prog2/tasktracker/projectmodels/JSONProjectOverviewTest.java
+++ app/src/test/java/ch/zhaw/prog2/tasktracker/projectmodels/JSONProjectOverviewTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 class JSONProjectOverviewTest {
     Project testproject1;
@@ -16,9 +18,12 @@ class JSONProjectOverviewTest {
     JSONProjectOverview projectOverview;
     @BeforeEach
     void setUp() {
-        testproject1 = mock(Project.class);
-        testproject2 = mock(Project.class);
+        // Using regular Project objects as Mock objects cannot be properly serialized and cause errors when saving to json
+        testproject1 = new Project("testproject1");
+        testproject2 = new Project("testproject2");
+        Path databasefile = Paths.get("databse.json");
         try {
+            Files.deleteIfExists(databasefile);
             projectOverview = new JSONProjectOverview();
         }catch (IOException e) {
             throw new RuntimeException(e);
@@ -45,9 +50,11 @@ class JSONProjectOverviewTest {
         projectOverview.addProject(testproject2);
         expected.add(testproject1);
         projectOverview.removeProject(testproject2);
+        System.out.println(expected);
         assertEquals(projectOverview.getProjectList(), expected);
         projectOverview.removeProject(testproject1);
         expected.remove(testproject1);
+        System.out.println(expected);
         assertEquals(projectOverview.getProjectList(), expected);
 
     }
