877a40c7a3c297425eeec59b43f131cb27c00e45
nussbjan <nussbjan@students.zhaw.ch>
2023-05-07 09:54:28 +0200

    remove calendar

--- app/src/test/java/ch/zhaw/prog2/tasktracker/project/ProjectTest.java
+++ app/src/test/java/ch/zhaw/prog2/tasktracker/project/ProjectTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
